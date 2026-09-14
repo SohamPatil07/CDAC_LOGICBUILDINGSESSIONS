b47f247fd3f6b6209ed1f2869ae2968f460221d7
Soham Patil <100117950+SohamPatil07@users.noreply.github.com>
2026-09-14 11:42:41 +0530

    Add files via upload

---
+++ AUGUST2026/Java/forloops/evennumber.java
@@ -0,0 +1,10 @@
+public class evennumber{
+    public static void main(String[] args){
+
+        for(int i=0;i<=500;i++){
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
